85a549cb65d6d86defd034c3ddbf04d8a8456f21
liehuo678 <137808720+liehuo678@users.noreply.github.com>
2024-04-27 19:06:34 +0800

    Update setup_pro.sh

--- scripts/setup_pro.sh
+++ scripts/setup_pro.sh
@@ -76,7 +76,7 @@ done
 
 # Clone the repository and install dependencies
 echo "curl -o docker-compose.yml..."
-curl -o docker-compose.yml https://raw.githubusercontent.com/Nanjiren01/AIChatWeb/main/docker-compose.yml
+curl -o docker-compose.yml https://raw.github.com/liehuo678/AIChatWeb/main/docker-compose.yml
 
 # Replace the image repository with the private registry
 if sed -i 's/nanjiren01\/aichat-/harbor.nanjiren.online:8099\/aichat\/aichat-/g' docker-compose.yml; then
